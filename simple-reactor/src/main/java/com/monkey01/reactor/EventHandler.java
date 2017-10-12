6f48ec024b4304c9c1b3da2373419c2c929fe94a
feiweiwei <feiweiwei@shrbank.com>
2017-10-12 16:43:09 +0800

    ＃新增reactor模式代码

---
+++ simple-reactor/src/main/java/com/monkey01/reactor/EventHandler.java
@@ -0,0 +1,21 @@
+package com.monkey01.reactor;
+
+/**
+ * @Author: feiweiwei
+ * @Description: event处理器的抽象类
+ * @Created Date: 11:26 17/10/12.
+ * @Modify by:
+ */
+public abstract class EventHandler {
+
+	private InputSource source;
+	public abstract void handle(Event event);
+
+	public InputSource getSource() {
+		return source;
+	}
+
+	public void setSource(InputSource source) {
+		this.source = source;
+	}
+}
